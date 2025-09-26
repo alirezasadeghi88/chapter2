2ca5f9650e0aab561c4f1ac6d54fe4bff2ef42cf
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-09-26 05:57:31 -0700

    Refactoring

--- ComputeArea.java
+++ ComputeArea.java
@@ -1,7 +1,13 @@
 public class ComputeArea {
 	
 	public static void main(String[] args){
+		double radius;
+		double area;
 		
+		radius = 20;
+		area = radius * radius * 3.14;
+		
+		System.out.println(area);
 	}
 
 }
