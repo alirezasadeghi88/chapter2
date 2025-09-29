2b18725f1a5d1a0c4e4e74b6798fefed4bb81dfc
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-09-29 11:43:57 -0700

    R

---
+++ Hello.java
@@ -0,0 +1,9 @@
+import java.util.Scanner; 
+
+public class Hello {
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+		String name=input.next();
+		System.out.print("Hello" + name);
+	}
+}
