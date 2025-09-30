8dffcac7701cdb5605e655e198e38dd3a70c35c0
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-09-30 07:41:04 -0700

    R

---
+++ Remainder.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; 
+
+ public class Remainder {
+	public static void main(String[] args) {
+ 
+	Scanner input = new Scanner(System.in);
+		double number1 = input.nextDouble();
+		double number2 = input.nextDouble();
+		System.out.print(number1/number2);
+	}
+}
